73e3fca2de2052341ceebdf122995d8adaf83b49
Viktoras Agejevas <v.agejevas@gmail.com>
2024-05-21 11:36:18 +0300

    Update stations

--- internal/radio/stations.go
+++ internal/radio/stations.go
@@ -23,9 +23,9 @@ const defaultStationsCSV = `SomaFM: Secret Agent,https://somafm.com/secretagent1
 	SomaFM: Fluid,http://somafm.com/fluid130.pls
 	SomaFM: Cliqhop IDM,https://somafm.com/cliqhop130.pls
 	SomaFM: Illinois Street Lounge,http://somafm.com/illstreet.pls
+	SomaFM: Underground 80s,https://somafm.com/u80s130.pls
 	SomaFM: Vaporwaves,http://somafm.com/vaporwaves.pls
 	SomaFM: Drone Zone,http://somafm.com/dronezone.pls
-	SomaFM: Deep Space One,https://somafm.com/deepspaceone130.pls
 	9128live,https://streams.radio.co/s0aa1e6f4a/listen
 	Chillsky,https://lfhh.radioca.st/stream
 	Nightride,https://stream.nightride.fm/nightride.ogg
@@ -35,10 +35,12 @@ const defaultStationsCSV = `SomaFM: Secret Agent,https://somafm.com/secretagent1
 	Lounge Motion FM,https://vm.motionfm.com/motionthree_aacp
 	Smooth Motion FM,https://vm.motionfm.com/motiontwo_aacp
 	Seeburg 1000,https://psn3.prostreaming.net/proxy/seeburg/stream/;
+	Classic Vinyl,http://icecast.walmradio.com:8000/classic
 	Jazz Groove,https://audio-edge-cmc51.fra.h.radiomast.io/f0ac4bf3-bbe5-4edb-b828-193e0fdc4f2f
 	KCSM Jazz Tonight,https://ice7.securenetsystems.net/KCSM2
 	Jazz24,https://prod-52-201-196-36.amperwave.net/ppm-jazz24aac256-ibc1
-	Classic Rock Florida,https://vip2.fastcast4u.com/proxy/classicrockdoug?mp=/1`
+	Classic Rock Florida,https://vip2.fastcast4u.com/proxy/classicrockdoug?mp=/1
+	Radio Paradise,http://stream.radioparadise.com/aac-128`
 
 func Stations(sta string) ([]string, []string) {
 	var scanner *bufio.Scanner
