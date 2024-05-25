2feb40f6776d4b3098293d71c097faf1d63cef30
Viktoras Agejevas <v.agejevas@gmail.com>
2024-05-25 23:24:12 +0300

    Update stations

--- internal/radio/stations.go
+++ internal/radio/stations.go
@@ -19,28 +19,55 @@ const defaultStationsCSV = `SomaFM: Secret Agent,https://somafm.com/secretagent1
 	SomaFM: Beat Blender,https://somafm.com/beatblender.pls
 	SomaFM: Bossa Beyond,https://somafm.com/bossa256.pls
 	SomaFM: Groove Salad Classic,https://somafm.com/gsclassic130.pls
-	SomaFM: DEF CON Radio,http://somafm.com/defcon.pls
+	SomaFM: DEF CON Radio,https://somafm.com/defcon256.pls
 	SomaFM: Fluid,http://somafm.com/fluid130.pls
-	SomaFM: Cliqhop IDM,https://somafm.com/cliqhop130.pls
+	SomaFM: Cliqhop IDM,https://somafm.com/cliqhop256.pls
 	SomaFM: Illinois Street Lounge,http://somafm.com/illstreet.pls
-	SomaFM: Underground 80s,https://somafm.com/u80s130.pls
 	SomaFM: Vaporwaves,http://somafm.com/vaporwaves.pls
-	SomaFM: Drone Zone,http://somafm.com/dronezone.pls
+	SomaFM: Drone Zone,https://somafm.com/dronezone256.pls
 	9128live,https://streams.radio.co/s0aa1e6f4a/listen
 	Chillsky,https://lfhh.radioca.st/stream
 	Nightride,https://stream.nightride.fm/nightride.ogg
 	Jungletrain.net,http://stream1.jungletrain.net:8000
-	Lounge Motion FM,https://vm.motionfm.com/motionthree_aacp
-	Smooth Motion FM,https://vm.motionfm.com/motiontwo_aacp
-	Seeburg 1000,https://psn3.prostreaming.net/proxy/seeburg/stream/;
+	Deepinside Radio Show,https://n44a-eu.rcs.revma.com/uyrbt6xuhnruv
+	Deepinside Guest Sessions,https://n30a-eu.rcs.revma.com/u62vcepz3tzuv
+	Deep Motion FM,https://vm.motionfm.com/motionone_free
+	Lounge Motion FM,https://vm.motionfm.com/motionthree_free
+	Smooth Motion FM,https://vm.motionfm.com/motiontwo_free
+	Magic Radio,http://mp3.magic-radio.net/
+	Classic Rock Florida,https://vip2.fastcast4u.com/proxy/classicrockdoug?mp=/1
 	Classic Vinyl,http://icecast.walmradio.com:8000/classic
-	Jazz Groove,https://audio-edge-cmc51.fra.h.radiomast.io/f0ac4bf3-bbe5-4edb-b828-193e0fdc4f2f
-	Jazz24,https://prod-52-201-196-36.amperwave.net/ppm-jazz24aac256-ibc1
+	The Jazz Groove: Mix #1,http://east-mp3-128.streamthejazzgroove.com/stream
+	The Jazz Groove: Mix #2,http://west-mp3-128.streamthejazzgroove.com/stream
+	Jazz24,https://live.amperwave.net/direct/ppm-jazz24aac256-ibc1
+	Linn Jazz,http://radio.linn.co.uk:8000/autodj
 	HiRes: City Radio Smooth & Jazz,http://cityradio.ddns.net:8000/cityradio48flac
 	HiRes: Radio Paradise,https://stream.radioparadise.com/flacm
 	HiRes: Radio Paradise Mellow,https://stream.radioparadise.com/mellow-flacm
 	HiRes: Radio Paradise Rock,https://stream.radioparadise.com/rock-flacm
-	Classic Rock Florida,https://vip2.fastcast4u.com/proxy/classicrockdoug?mp=/1`
+	HiRes: JB Radio2,https://maggie.torontocast.com:8076/flac
+	HiRes: MaXXima,http://maxxima.mine.nu:8000/maxx.ogg
+	HiRes: Radio Sputnik Underground!,https://radiosputnik.nl:8443/flac
+	HiRes: SomaFM: Groove Salad,https://hls.somafm.com/hls/groovesalad/FLAC/program.m3u8
+	HiRes: Naim Radio,http://mscp3.live-streams.nl:8360/flac.flac
+	HiRes: Naim Jazz,http://mscp3.live-streams.nl:8340/jazz-flac.flac
+	HiRes: Naim Classical,http://mscp3.live-streams.nl:8250/class-flac.flac
+	HiRes: SuperStereo 1,http://icecast.centaury.cl:7570/SuperStereoHiRes1
+	HiRes: SuperStereo 2,http://icecast.centaury.cl:7570/SuperStereoHiRes2
+	HiRes: SuperStereo 3,http://icecast.centaury.cl:7570/SuperStereoHiRes3
+	HiRes: SuperStereo 4,http://icecast.centaury.cl:7570/SuperStereoHiRes4
+	HiRes: SuperStereo 5,http://icecast.centaury.cl:7570/SuperStereoHiRes5
+	HiRes: SuperStereo 6,http://icecast.centaury.cl:7570/SuperStereoHiRes6
+	HiRes: SuperStereo 7,http://icecast.centaury.cl:7570/SuperStereoHiRes7
+	HiRes: ∏ano,https://stream.p-node.org/piano.flac
+	Mother Earth Jazz,https://motherearth.streamserver24.com/listen/motherearth_jazz/motherearth.jazz.mp4
+	Mother Earth Instrumental,https://motherearth.streamserver24.com/listen/motherearth_instrumental/motherearth.instrumental.aac
+	Mother Earth Radio,https://motherearth.streamserver24.com/listen/motherearth/motherearth.aac
+	Mother Earth Klassic,https://motherearth.streamserver24.com:18910/motherearth.klassik.aac
+	Linn Classical,http://radio.linn.co.uk:8004/autodj
+	Linn Radio: http://radio.linn.co.uk:8003/autodj
+	Jammin Vibez Radio,https://azuracast.jammimvibez.com/listen/classics/stream
+	Seeburg 1000,https://psn3.prostreaming.net/proxy/seeburg/stream/;`
 
 func Stations(sta string) ([]string, []string) {
 	var scanner *bufio.Scanner
